8ab06dd170ee657f9c12b931d9bb69082729a088
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-08-15 14:13:42 +0300

    Add: Models and migration

---
+++ blog/app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['title', 'content', 'user_id', 'category_id'];
+
+    // Define relationships
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+    use HasFactory;
+}
