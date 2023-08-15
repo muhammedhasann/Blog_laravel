3f1b3caaa29aca57f9788797dcb60de2b3f9c8b9
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-08-15 16:13:16 +0300

    Add: roles and permission

--- blog/app/Models/User.php
+++ blog/app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use HasRoles;
+
 
 class User extends Authenticatable
 {
